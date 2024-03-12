685e63610711674c010437e3ea0f8fb515087da0
yuchengH1988 <yucheng.h.1988@gmail.com>
2024-03-12 11:58:13 +0800

    C229 typing our payment form

--- src/routes/category/category.component.tsx
+++ src/routes/category/category.component.tsx
@@ -8,8 +8,14 @@ import Spinner from '../../components/spinner/spinner.component';
 import { CategoryContainer, Title } from './category.styles';
 import ProductCard from '../../components/product-card/product-card.component';
 
+type CategoryRouteParams = {
+  category: string
+}
+
 const Category = () => {
-  const { category } = useParams();
+  const { category } = useParams<
+    keyof CategoryRouteParams
+  >() as CategoryRouteParams;
   const categoriesMap = useSelector(selectCategoriesMap);
   const isLoading = useSelector(selectCategoriesIsLoading);
   const [products, setProducts] = useState(categoriesMap[category]);
